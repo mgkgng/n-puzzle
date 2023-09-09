5bdab5216916376930b040efe24d519d294ff506
ddupont06 <dorian.dupont@hotmail.fr>
2023-09-09 12:10:36 +0200

    added_comments_algo

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -24,46 +24,45 @@ static bool isGoal(const pState &s) {
     return s.board == puzzle->goalGrid;
 }
 
-// bound = threshold
-static int search(pState &s, int g, int bound, int hChoice, vector<char> &solution) {
+static int search(pState &s, int g, int threshold, int hChoice, vector<char> &solution) {
     int f = g + hFunction(s, hChoice);
 
-    if (f > bound)
+    if (f > threshold) // Current branch should be pruned because it exceeds the threshold
         return f;
 
-    if (isGoal(s))
-        return -1;
+    if (isGoal(s)) // Solution found
+        return -1; 
 
-    int minNextBound = numeric_limits<int>::max();
+    int minNextThreshold = numeric_limits<int>::max();
 
-    int dx[] = {-1, 1, 0, 0};
-    int dy[] = {0, 0, -1, 1};
-    char moves[] = {'U', 'D', 'L', 'R'};
+    int dx[] = {-1, 1, 0, 0}; // Possible moves in the x cartesian axis
+    int dy[] = {0, 0, -1, 1}; // Possible moves in the y cartesian axis
+    char moves[] = {'U', 'D', 'L', 'R'}; // Char representation of the moves
 
-    for (int i = 0; i < 4; ++i) {
-        if (move(dx[i], dy[i], s)) {
-            int newBound = search(s, g + 1, bound, hChoice, solution);
-            if (newBound == -1) {
-                solution.push_back(moves[i]); // Add the move to the solution
-                return -1; // Solution found in the subtree
+    for (int i = 0; i < 4; ++i) { // For each possible move
+        if (move(dx[i], dy[i], s)) { // If the move is valid apply it to the state
+            int newThreshold = search(s, g + 1, threshold, hChoice, solution); // Recursively call the search function with the new state
+            if (newThreshold == -1) { // Solution found in the subtree
+                solution.push_back(moves[i]); // Add the move to the solution vector
+                return -1;
             }
-            if (newBound < minNextBound)
-                minNextBound = newBound;
-            move(-dx[i], -dy[i], s); // Undo the move
+            if (newThreshold < minNextThreshold) // Update the minimum threshold
+                minNextThreshold = newThreshold;
+            move(-dx[i], -dy[i], s); // Undo the move to restore the state to i
         }
     }
 
-    return minNextBound;
+    return minNextThreshold;
 }
 
 int IDAstar(pState &initialState, int hChoice, vector<char> &solution) {
-    int bound = hFunction(initialState, hChoice);
+    int threshold = hFunction(initialState, hChoice); // Initial threshold
     while (true) {
-        int nextBound = search(initialState, 0, bound, hChoice, solution);
-        if (nextBound == -1)
-            return bound; // Solution found
-        if (nextBound == numeric_limits<int>::max())
-            return -1; // No solution found
-        bound = nextBound;
+        int nextThreshold = search(initialState, 0, threshold, hChoice, solution); // Search for a solution with the current threshold
+        if (nextThreshold == -1) // Solution found
+            return threshold;
+        if (nextThreshold == numeric_limits<int>::max()) // No solution found
+            return -1;
+        threshold = nextThreshold; // Update the threshold
     }
 }
